90447fa6582880dc3d36b136aa0aec01c107056c
Ronnie Laugen <rhenri@equinor.com>
2021-09-01 11:48:25 +0200

    Add more debug-output

--- index.ts
+++ index.ts
@@ -55,6 +55,8 @@ program.parse(process.argv);
       if (ex.status === 404) {
         core.setOutput('exists', false);
       } else {
+        core.info(JSON.stringify(ex));
+        core.info(JSON.stringify(process.env))
         core.setFailed(`Invalid response from Radix, expected 20x or 404, got ${ex.status}`);
       }
     }
